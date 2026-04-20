090753d21299e9e2064ff1ec554533bf9e1da62d
Vihashini <nvihashinibrindha@gmail.com>
2026-04-20 08:23:41 +0530

    Delete DEMO directory

--- DEMO/haptic.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAPTIC_H
-#define HAPTIC_H
-
-#include <Arduino.h>
-void haptic_init();
-void haptic_trigger(String pattern, String priority);
-
-#endif
--- DEMO/speaker.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUDIO_MODULE_H
-#define AUDIO_MODULE_H
-
-#include <Arduino.h>
-
-void audio_init();
-void audio_play(String message_id);
-void audio_loop();
-#endif
